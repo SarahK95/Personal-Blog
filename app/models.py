83e2d94f61e3d0c4577ce990e90116cbe9c6ce9d
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-15 23:41:37 +0300

    create class for posts

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class User():
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    
+    
+class Post():
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
