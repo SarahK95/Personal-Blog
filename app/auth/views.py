91d5de2bdc694c241501a3e67f0482dfd1a964fb
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-20 01:37:04 +0300

    add route for logout, logout function for users

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,5 +28,11 @@ def login():
         flash("Invalid Username or Password")
         title = "Please Log in"
         return render_template("auth/login.html",login_form = login_form,title = title)
+    
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))    
         
         
