f9c390642ab5590218ab508fafd3dea1d7fdf3ca
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-20 02:11:59 +0300

    add updateblog class, commentform class, update profile class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,26 @@
+import email
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField,SubmitField, SelectField)
 from wtforms.validators import Required
 
 class BlogForm(FlaskForm):
     title = StringField("Blog title:", validators=[Required()])
-    blog = TextAreaField("Type Away:", validators=[Required()])
-    submit = SubmitField("Post")
+    blog = TextAreaField("Input text:", validators=[Required()])
+    submit = SubmitField("Blog")
+    
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Blog Comment", validators=[Required()])
+    alias = StringField("Comment Alias")
+    submit = SubmitField("Comment")
+    
+class UpdateBlogForm(FlaskForm):
+    title = StringField("Blog title", validators=[Required()])
+    blog =  TextAreaField("Input text:", validators=[Required()])
+    submit = SubmitField("Update")
+    
+class UpdateProfile(FlaskForm):  
+    first_name = StringField("First name")
+    last_name = StringField("Last name")
+    bio = TextAreaField("Bio")
+    email = StringField("Email")
+    submit = SubmitField("Update")  
