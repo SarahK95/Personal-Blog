ab5916b8899c25d933b9df4048de9e9565b65daf
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-20 01:42:02 +0300

    add class loginform users login with username and email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, SubmitField, ValidationError, BooleanField)
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Your Email Address", validators=[Required(), Email()])
+    password = PasswordField("Password", validators=[Required()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
