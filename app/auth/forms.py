4f637c5c0aac4e87c7adc645d2bc4e60a1b8a8a0
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-20 01:46:35 +0300

    add class signupform, for first time user to sign in

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,13 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[Required()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+    
+    
+class SignUpForm(FlaskForm):
+    first_name = StringField("Your First Name", validators=[Required()])
+    last_name = StringField("Your Last Name", validators=[Required()])    
+    username = StringField("Your Username", validators=[Required()])
+    email = StringField("Your Email Address", validators=[Required(), Email()])
+    password = PasswordField("Password", validators=[Required(), EqualTo("password_confirm", message = "Passwords must match")])
+    password_confirm = PasswordField("Confirm Password", validators=[Required()])
+    submit = SubmitField("Sign Up")
