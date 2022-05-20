c0a5ba7d371c54f2a1d7fc2e83dc377492430981
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-20 05:41:47 +0300

    add sqlalchemy uri to start.sh

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
+import os
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+    debug = True
+    SECRET_KEY = 'secret'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
 
-
-
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -19,9 +26,12 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
